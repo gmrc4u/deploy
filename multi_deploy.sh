7d9de97e17ffe74e9953196ed5c14d2daf4a90c4
gmrc4u <48318352+gmrc4u@users.noreply.github.com>
2019-04-27 22:06:29 +0530

    new repo created

---
+++ multi_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $ENVIRONMENT = "QA" ];then
+	for i in `cat ips.txt`
+	do
+#
+	echo "Deploying war file into $i QA server.."
+	sleep 3
+	sshpass -p "kalyan" scp target/gamutkart.war kalyan@$i:/home/kalyan/Distros/apache-tomcat-8.5.38/webapps
+#
+	echo "Starting tomcat server in $i QA server.."
+	sshpass -p "gamut" ssh gamut@$i "JAVA_HOME=/home/kalyan/soft/jdk1.8.0_201" "/home/kalyan/soft/apache-tomcat-9.0.17/bin/startup.sh"
+#
+	done
+	echo "Deployment is succussful!"
+fi
